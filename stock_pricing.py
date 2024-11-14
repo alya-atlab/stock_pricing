6d41ace9d22bee6cc621d042fccb37990ac33b71
Alya Atlab <133522435+alya-atlab@users.noreply.github.com>
2024-11-14 15:45:26 +0200

    Add comments

--- stock_pricing.py
+++ stock_pricing.py
@@ -1,15 +1,17 @@
 def max_profit(prices):
     # min_price = min(prices)
     # max_price = max(prices[prices.index(min_price) :])
-    count = 0
-    profit = []
+    count = 0  # for the index
+    profit = []  # the max profit for each day
     for i in prices:
         min_price = i
-        max_price = max(prices[count:])
+        max_price = max(
+            prices[count:]
+        )  # calculate the max price from the current day to the end
         profit.append(max_price - min_price)
         count = count + 1
 
-    return max(profit)
+    return max(profit)  # return the max profit
 
 
 print(max_profit([2, 2, 3, 1, 4, 2, 4, 6, 1, 4]))
