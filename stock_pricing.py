855b6ffb16d2415169f10ab9c4041c75f0d4af1a
Alya Atlab <133522435+alya-atlab@users.noreply.github.com>
2024-11-14 15:39:29 +0200

    Edit the function

--- stock_pricing.py
+++ stock_pricing.py
@@ -1,7 +1,16 @@
 def max_profit(prices):
-    min_price = min(prices)
-    max_price = max(prices[prices.index(min_price) :])
-    return max_price - min_price
+    # min_price = min(prices)
+    # max_price = max(prices[prices.index(min_price) :])
+    count = 0
+    profit = []
+    for i in prices:
+        min_price = i
+        max_price = max(prices[count:])
+        profit.append(max_price - min_price)
+        count = count + 1
+
+    return max(profit)
 
 
 print(max_profit([2, 2, 3, 1, 4, 2, 4, 6, 1, 4]))
+print("example 2:", max_profit([3, 10, 2, 4, 7]))
